ee6f7caf5511709a406079c67b34ed44efe189db
v_guangliu <v_guangliu@jintengtech.com>
2021-08-20 16:54:18 +0800

    feat[config change]:delete head

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -4,15 +4,11 @@ import type { DefaultThemeOptions } from 'vuepress';
 export default defineUserConfig<DefaultThemeOptions>({
   title: "matLiu's blog",
   description: 'matLiu的个人网站',
-  head: [
-    // 注入到当前页面的 HTML <head> 中的标签
-    ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
-  ],
   base: '/', // 这是部署到github相关的配置
 
   // 主题配置
   themeConfig: {
-    logo: 'https://vuejs.org/images/logo.png',
+    // logo: 'https://vuejs.org/images/logo.png',
     nav: [
       // 导航栏配置
       { text: '前端基础', link: '/accumulate/' },
